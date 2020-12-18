020e1ab9dc5ff9922faacd47c1c6e888eab288eb
awais381 <76220531+awais381@users.noreply.github.com>
2020-12-18 21:46:43 +0500

    commit

--- index.php
+++ index.php
@@ -35,16 +35,16 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/PakWebsite/index.php">Home <span class="sr-only">(current)</span></a>
       </li>
 
       <li class="nav-item">
-        <a class="nav-link" href="about.php">About</a>
+        <a class="nav-link" href="/PakWebsite/about.php">About</a>
       </li>
        
 
       <li class="nav-item">
-        <a class="nav-link" href="contact.php">Contact</a>
+        <a class="nav-link" href="/PakWebsite/contact.php">Contact</a>
       </li>
 
 
